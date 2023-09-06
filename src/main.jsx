8f4c201ee186bb0486536217e414837aabb2eda4
talzeraviv <talzeraviv3@gmail.com>
2023-09-06 10:36:20 +0300

    Added .env

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import "./index.css";
 import axios from "axios";
 import { StoreProvider } from "./Context/StoreProvider.jsx";
 
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
