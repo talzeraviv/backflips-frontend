acdfa3ec16640a0e631d873154eb7230ea040dbc
talzeraviv <talzeraviv3@gmail.com>
2023-09-27 09:44:18 +0300

    Fixed dev to prod backend connection.

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import axios from "axios";
 import { StoreProvider } from "./Context/StoreProvider.jsx";
 
 // axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
-axios.defaults.baseURL = "http://localhost:5000/api";
+axios.defaults.baseURL =
+  import.meta.env.VITE_BASE_URL || "http://localhost:5000/api";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
